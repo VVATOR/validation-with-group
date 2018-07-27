f52332940665dc2bb99dbf8f345fae2318e9f074
Vitali Vikhliayeu <vitali_vikhliayeu@epam.com>
2018-07-27 17:58:25 +0300

    change configuration

--- src/main/java/com/epam/grow/Runner.java
+++ src/main/java/com/epam/grow/Runner.java
@@ -3,14 +3,18 @@ package com.epam.grow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-
+@EnableWebMvc
 @EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication(scanBasePackages = {"com.epam.grow"})
 public class Runner {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     SpringApplication.run(Runner.class, args);
   }
+
 
 }
--- src/main/java/com/epam/grow/controllers/ValidationController.java
+++ src/main/java/com/epam/grow/controllers/ValidationController.java
@@ -2,9 +2,11 @@ package com.epam.grow.controllers;
 
 
 import com.epam.grow.dto.Person;
-
 import javax.validation.Validator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,12 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/")
 public class ValidationController {
 
+  private static final Logger LOG = LoggerFactory.getLogger(ValidationController.class);
+
   @Autowired
   Validator validator;
 
   @RequestMapping(value = "/valid/1", method = RequestMethod.POST)
-  public void validCase1(@RequestBody Person person){
+  public void validCase1(@RequestBody Person person) {
+    validator.validate(person);
+    System.out.println("validCase1: " + validator.validate(person));
+  }
 
+  @RequestMapping(value = "/valid/3", method = RequestMethod.POST)
+  public void validCase2(@RequestBody @Validated Person person) {
+    validator.validate(person);
+    LOG.info("validCase3: " + validator.validate(person));
   }
 
 
